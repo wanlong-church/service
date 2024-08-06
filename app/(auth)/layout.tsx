f7e360854cd30a087f75439c3c23dd31e0f00a0d
Schwannden Kuo <schwannden@gmail.com>
2024-08-06 21:00:48 +0800

    feat(route): adding custom domain

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
   return (
     <div className="flex h-screen max-h-screen w-screen flex-col overflow-y-auto bg-white">
       <PageTitle />
-      <div className="h-[calc(100svh-90px)] overflow-auto">{children}</div>
+      <div className="h-[calc(100svh-143px)] overflow-auto">{children}</div>
       <div className="border-t bg-gray-200 p-3">
         <Nav />
       </div>
