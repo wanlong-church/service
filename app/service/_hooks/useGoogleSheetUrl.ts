71d9a532dd8f1c623517bb6602449e919370f08d
Schwannden Kuo <schwannden@gmail.com>
2024-10-02 15:10:27 +0800

    fix(ui): longer cache
    * last pr forgotto remove the cache staleTime overwrite
    * make sure data is invalidated if sheet changes

--- app/service/_hooks/useGoogleSheetUrl.ts
+++ app/service/_hooks/useGoogleSheetUrl.ts
@@ -6,8 +6,6 @@ import { GoogleSheetUrlResponse } from '@/app/type'
 export default function useGoogleSheetUrl() {
   const query = useQuery<GoogleSheetUrlResponse, Error>({
     queryKey: [QUERY_KEY.GOOGLE_SHEET_URL],
-    staleTime: Infinity,
-    gcTime: Infinity,
     queryFn: fetchGoogleSheetUrl,
     select: (data) => ({ thisYearUrl: data.thisYearUrl, nextYearUrl: data.nextYearUrl }),
     retry: 3,
